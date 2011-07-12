2dc566c8614eb344ea7d129b76d95850af51915d
John Howe (SGS) <jhowe@seattlegirlsschool.org>
2011-07-12 15:24:13 -0700

    Added Battery scanning and red/green status

--- audit.rb
+++ audit.rb
@@ -4,7 +4,7 @@
 # While we are at it, do some basic hardware auditing
 # Gather it all up and send audit info to a HTTP(s) cache
 
-# Puppet clues: sudo /usr/bin/gem install yajl-ruby
+# Puppet clues: sudo /usr/bin/gem install yajl-ruby colored
 
 # Based on work from http://glarizza.posterous.com/43331766
 
@@ -14,6 +14,7 @@ require 'timeout'
 # require 'uri'
 require 'rubygems'
 require 'yajl'
+require 'colored'
 
 # Methudium
 
@@ -56,6 +57,14 @@ def hardware_info_long(query)
   %x(system_profiler SPHardwareDataType | awk -F': ' '/#{query}/ {print $2,$3,$4}').chomp
 end
 
+def battery_info_short(query)
+  %x(system_profiler SPPowerDataType | awk -F': ' '/#{query}/ {print $2}').chomp
+end
+
+def battery_info_long(query)
+  %x(system_profiler SPPowerDataType | awk -F': ' '/#{query}/ {print $2,$3,$4}').chomp
+end
+
 def report(serial, result)
 
   model_id = hardware_info_short("Model Identifier")
@@ -70,6 +79,11 @@ def report(serial, result)
   boot_rom = hardware_info_short("Boot ROM Version")
   hardware_uuid = hardware_info_long("Hardware UUID")
 
+  batt_cycle_count = battery_info_short("Cycle count")
+  batt_condition = battery_info_long("Condition").rstrip
+  batt_charge_remaining = battery_info_short("Charge remaining")
+  batt_charge_capacity = battery_info_short("Full charge capacity")
+
   puts   "Serial Number:    " + result["SERIAL_ID"]
   puts   "Hardware UUID:    " + hardware_uuid
   puts   "Model Type:       " + result["PROD_DESCR"]
@@ -80,6 +94,23 @@ def report(serial, result)
   puts   "Processor Cores:  " + proc_cores
   puts   "Installed RAM:    " + ram
   puts   "L2 Cache:         " + l2_cache
+
+  if batt_cycle_count.to_i >= 300
+    puts "Battery Count:    " + batt_cycle_count.red
+  elsif batt_cycle_count.to_i >= 250
+    puts "Battery Count:    " + batt_cycle_count.yellow
+  else
+    puts "Battery Count:    " + batt_cycle_count.green
+  end
+
+  if batt_condition == "Replace Now"
+    puts "Battery Status:   " + batt_condition.red
+  elsif batt_condition == "Normal"
+    puts "Battery Status:   " + batt_condition.green
+  else
+    puts "Battery Status:   " + batt_condition.yellow
+  end
+
   puts   "Bus Speed:        " + bus_speed
   puts   "SMC Version:      " + smc_version
   puts   "Boot ROM:         " + boot_rom
@@ -87,11 +118,11 @@ def report(serial, result)
 
   if ( result["HW_HAS_COVERAGE"] == "Y" ) then
     # HW_END_DATE =~ COV_END_DATE
-    puts "Coverage Expires: " + result["COV_END_DATE"]
-    puts "Repair Days Left: " + result["DAYS_REM_IN_COV"]
+    puts "Coverage Expires: " + result["COV_END_DATE"].green
+    puts "Repair Days Left: " + result["DAYS_REM_IN_COV"].green
   else
-    puts "System Out of Coverage!"
-    puts "System Age (Yrs): " + (result["NUM_DAYS_SINCE_DOP"].to_i/365.0).to_s
+    puts "System Out of Coverage!".red
+    puts "System Age (Yrs): " + (result["NUM_DAYS_SINCE_DOP"].to_i/365.0).to_s.red
   end
 
   network_info

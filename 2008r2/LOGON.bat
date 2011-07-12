1a5cef28e40682c1c5b655b070bbff50541c1359
John Howe <jhowe@seattlegirlsschool.org>
2011-07-12 16:39:29 -0700

    Revert c792dc45a11a83333c934e43f249d12c65b12e84^..HEAD

--- 2008r2/LOGON.bat
+++ 2008r2/LOGON.bat
@@ -1,35 +1,16 @@
-@ECHO OFF
-NET USE * /delete /y
+REM @ECHO OFF
+REM NET USE * /delete /y
+REM
 
 REM http://clintboessen.blogspot.com/2011/02/ifmemberexe-doesnt-work-windows.html
 
 :PUBLIC
 
 NET USER /domain %username% | find "Public Shares"
-IF NOT ERRORLEVEL 1 (
+IF NOT ERRORLEVEL = 1 (
   NET USE P: \\LAKSHMI\public
 )
 
-:QUICKBOOKS
-
-NET USER /domain %username% | find "Quickbooks Shares"
-IF NOT ERRORLEVEL  1 (
-  NET USE Q: \\YOMEJA\quickbooks
-)
-
-
-:ACADEMICS
-
-:ADMISSIONS
-
-:DEVELOPMENT
-
-:LEADERSHIP
-
-:FACULTY
-
-:PAYROLL
-
-:STUDENTS
+GOTO END
 
 :END

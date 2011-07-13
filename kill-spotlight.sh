9c1343ff9a74b5f678d657cca44a1a8db60c8f82
John Howe <jhowe@seattlegirlsschool.org>
2011-07-12 21:23:19 -0700

    Consolidate files, add some bass checks

--- kill-spotlight.sh
+++ kill-spotlight.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+if [ ${EUID} -ne 0 ]; then
+  echo
+  echo "You don't seem to be an old tree yet... Got root?"
+  echo
+  exit 1
+fi
+
 echo "FOADIAF Snow Leopard Search!!!!"
-sudo mdutil -a -i off
-sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
-sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
-sudo killall SystemUIServer
+mdutil -a -i off
+launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
+killall SystemUIServer
 echo "Long Live QuickSilver!!!"

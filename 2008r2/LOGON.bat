afc67333b0e4a923060c2c475289868c38d986e7
John Howe <jhowe@seattlegirlsschool.org>
2011-07-12 17:44:25 -0700

    Completed group mapping

--- 2008r2/LOGON.bat
+++ 2008r2/LOGON.bat
@@ -4,7 +4,7 @@ NET USE * /delete /y
 @ECHO **************************************************************************
 @ECHO *                                                                        *
 @ECHO * Mapping SGS Network Drives. Please do not close this window.           *
-@ECHO * This window will automatically close when mapping is completed.        *
+@ECHO * This window will automatically close when mapping is complete.         *
 @ECHO *                                                                        *
 @ECHO **************************************************************************
 
@@ -22,4 +22,48 @@ IF NOT ERRORLEVEL 1 (
   NET USE P: \\YEMOJA\quickbooks
 )
 
+:BUSINESS
+
+NET USER /domain %username% | find "Business Shares"
+IF NOT ERRORLEVEL 1 (
+  NET USE N: \\YEMOJA\business
+)
+
+:HR
+
+NET USER /domain %username% | find "Human Resources Shares"
+IF NOT ERRORLEVEL 1 (
+  NET USE R: \\YEMOJA\human-resources
+)
+
+:ACADEMICS
+
+NET USER /domain %username% | find "Academics Shares"
+IF NOT ERRORLEVEL 1 (
+  NET USE X: \\LAKSHMI\academics
+)
+
+:ADMISSIONS
+
+NET USER /domain %username% | find "Admissions Shares"
+IF NOT ERRORLEVEL 1 (
+  NET USE X: \\LAKSHMI\admissions
+)
+
+:DEVELOPMENT
+
+NET USER /domain %username% | find "Development Shares"
+IF NOT ERRORLEVEL 1 (
+  NET USE X: \\LAKSHMI\development
+)
+
+:LEADERSHIP
+
+NET USER /domain %username% | find "Leadership Shares"
+IF NOT ERRORLEVEL 1 (
+  NET USE X: \\LAKSHMI\leadership
+)
+
+:FACULTY
+:STUDENTS
 :END

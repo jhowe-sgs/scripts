d8f57c024ad96e50d6d5310beab382455760f035
John Howe (SGS) <serveradmin@wenchang.seattlegirlsschool.org>
2013-10-08 16:43:09 -0700

    Build Notes

---
+++ MacOSX/MountainLion/build-java-webstart-pkg.sh
@@ -0,0 +1,7 @@
+http://derflounder.wordpress.com/2012/08/15/creating-payload-free-packages-with-pkgbuild/
+
+http://www.wikihow.com/Make-a-DMG-File-on-a-Mac
+
+http://macops.ca/flat-packages-persisting-obsolescence/
+
+sudo pkgbuild --version 2013100801 --identifier org.seattlegirlsschool.enable_java_webstart --nopayload --scripts /Users/serveradmin/Desktop/github/scripts/MacOSX/MountainLion/Enable_Java_Webstart/scripts "/Users/serveradmin/Desktop/Enable Java Webstart.pkg"

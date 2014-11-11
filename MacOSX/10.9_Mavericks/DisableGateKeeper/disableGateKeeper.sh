f445e6c86e335edf21ab3719c608b894cb66c1e5
John Howe <jhowe@seattlegirlsschool.org>
2014-11-11 10:58:10 -0800

    Folder Organization, comments past the shell call

---
+++ MacOSX/10.9_Mavericks/DisableGateKeeper/disableGateKeeper.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This script will change the Gatekeeper setting to allow 
+# applications downloaded from anywhere.
+/usr/sbin/spctl --master-disable
--- MacOSX/10.9_Mavericks/disableGateKeeper.sh
+++
@@ -1,4 +0,0 @@
-#This script will change the Gatekeeper setting to allow applications downloaded from anywhere.
-
-#!/bin/sh
-/usr/sbin/spctl --master-disable

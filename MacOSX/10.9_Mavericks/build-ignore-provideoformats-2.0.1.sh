293e65057b52575e8d7cc7cc93b365e6d09f86a0
John Howe - SGS <jhowe@seattlegirlsschool.org>
2015-04-28 12:22:45 -0700

    Silence Apple SUS Pro Video Formats 2.0.1 Infinite Installer for Now

---
+++ MacOSX/10.9_Mavericks/build-ignore-provideoformats-2.0.1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo pkgbuild \
+	--version 2015042801 \
+	--identifier org.seattlegirlsschool.ignore_provideoformats_2_0_1 \
+	--install-location /Applications/Utilities \
+	--component /Users/jhowe/Desktop/__Apps/Ignore_ProVideoFormats_2_0_1.app \
+	--scripts /Users/jhowe/github/scripts/MacOSX/10.9_Mavericks/Ignore_ProVideoFormats_2.0.1/scripts \
+	"/Users/jhowe/Desktop/__Apps/Ignore_ProVideoFormats_2_0_1.pkg"
